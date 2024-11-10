2426af48b900a41f283b3d3a4d30839b7632b59e
jjayaraaj <jjayaraaj@gmail.com>
2024-11-10 11:56:29 +0530

    Test update

--- src/app/dashboard/_component/NavBar.tsx
+++ src/app/dashboard/_component/NavBar.tsx
@@ -1,26 +1,22 @@
 import React from "react";
 
-import { 
-  ChatBubbleLeftEllipsisIcon, 
-  UserGroupIcon, 
-  Cog6ToothIcon, 
-
+import {
+  ChatBubbleLeftEllipsisIcon,
+  UserGroupIcon,
+  Cog6ToothIcon,
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
 
-
 export const Nav = () => {
-
-
-
-
   const mainNavItems = [
-    { to: "/add-feedback", icon: ChatBubbleLeftEllipsisIcon, label: "Add Feedback" },
+    {
+      to: "/add-feedback",
+      icon: ChatBubbleLeftEllipsisIcon,
+      label: "Add Feedback",
+    },
     { to: "/team", icon: UserGroupIcon, label: "Team" },
     { to: "/settings", icon: Cog6ToothIcon, label: "Settings" },
   ];
-
-
 
   return (
     <nav className="nav ">
@@ -31,19 +27,13 @@ export const Nav = () => {
       <div className="nav-links-container">
         <div className="nav-links">
           {mainNavItems.map((item) => (
-            <Link
-              key={item.to}
-              href={item.to}
-             className="nav-link"
-             
-            >
+            <Link key={item.to} href={item.to} className="nav-link">
               {item.icon && <item.icon className="nav-link-icon" />}
               {item.label}
             </Link>
           ))}
         </div>
       </div>
-      
     </nav>
   );
 };
