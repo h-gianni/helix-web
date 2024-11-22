3681f1d30bc28b76222cc1f56da7479434441260
jjayaraaj <jjayaraaj@gmail.com>
2024-11-22 15:14:51 +0530

    update test

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -5,7 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider>
-      <div>
+      <div className="test">
         <div className="grid grid-cols-12">
           <div className="col-span-3">
             <Nav />
