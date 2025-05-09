c48d855b5c90b8f99c2cce51de1193d9899d3fbc
ssssaravanakumarn <career.saravanakumar@gmail.com>
2025-05-09 14:25:04 +0530

    api timeout

--- src/lib/api/api-client.ts
+++ src/lib/api/api-client.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const apiClient = axios.create({
   baseURL: "/api",
-  timeout: 15000,
+  timeout: 60000,
   headers: {
     "Content-Type": "application/json",
   },
